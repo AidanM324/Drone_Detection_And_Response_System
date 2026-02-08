56d3b966114bf9a3a49c35b40a86694d26349c2a
Aidan Malone <G00423306@atu.ie>
2026-02-08 12:52:18 +0000

    modified main and stream to work with the raw camera logging

--- stream.py
+++ stream.py
@@ -1,29 +1,45 @@
 import cv2
 import logging
 import traceback
+import time 
+from datetime import datetime
 
-def mjpeg_generator(camera, detection=None, imgsz=640, conf=0.25, csv_writer=None, csvfile=None):
+def mjpeg_generator(camera, detection=None, raw_logger=None, imgsz=640, conf=0.25, csv_writer=None, csvfile=None):
+    frame_id = 0
+    prev_time = time.time()
+
     try:
         while True:
             frame = camera.read()
+            frame_id += 1
+
+            timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+
+            # FPS calculation
+            current_time = time.time()
+            fps = 1.0 / (current_time - prev_time)
+            prev_time = current_time
+
+            if raw_logger:
+                raw_logger.log(timestamp, frame_id, fps)
 
             if detection:
                 annotated, data = detection.annotate(frame, imgsz=imgsz, conf=conf)
 
                 if csv_writer is not None:
                     csv_writer.writerow([
-                        data["timestamp"],
-                        data["frame_id"],
-                        data["detected"],
-                        data["confidence"],
-                        data["x1"],
-                        data["y1"],
-                        data["x2"],
-                        data["y2"],
-                        data["area"],
-                        data["fps"]
-                    ])
-                    csvfile.flush()
+                       data["timestamp"],
+                       data["frame_id"],
+                       data["detected"],
+                       data["confidence"],
+                       data["x1"],
+                       data["y1"],
+                       data["x2"],
+                       data["y2"],
+                       data["area"],
+                       data["fps"]
+                   ])
+                csvfile.flush()
                     
                 out = annotated
             else:
