297f7f08caed3de9983e94e13823f0fa5ba4ae3d
Aidan Malone <G00423306@atu.ie>
2026-02-24 12:11:19 +0000

    Made updates to detection script, for min bounding box area and temporal 3 frame persistence.  Will have to iron out some issues

--- detection.py
+++ detection.py
@@ -6,9 +6,13 @@ from ultralytics import YOLO
 
 
 class DroneDetector:
-    def __init__(self, model_path: str):
+    def __init__(self, model_path: str, min_area = 500, confirm_frames = 3):
         self.model = YOLO(model_path)
         self.frame_id = 0
+        self.min_area = min_area
+        self.confirm_frames = confirm_frames
+
+        self.persistence_counter = 0
         self.prev_time = time.time()
         logging.info("Model loaded: %s", model_path)
 
@@ -35,19 +39,34 @@ class DroneDetector:
         results = self.model.predict(rgb, imgsz=imgsz, conf=conf, verbose=False)
         boxes = results[0].boxes
 
-        if len(boxes) > 0:
-            #logging.info("Detected %d object(s)", len(boxes))
-            #for box in boxes:
-            detected = 1
-            #cls_id = int(box.cls[0])
-            box = boxes[0]
-            confidence = float(box.conf[0])
-            #name = self.model.names.get(cls_id, str(cls_id))
-            x1, y1, x2, y2 = box.xyxy[0].tolist()
-            width = x2 - x1
-            height = y2 - y1
-            area = (width) * (height)
-            logging.info("Frame=%d Conf=%.2f Area=%.0f", self.frame_id, confidence, area)
+        for r in results:
+
+            if len(boxes) > 0:
+                #logging.info("Detected %d object(s)", len(boxes))
+                #for box in boxes:
+                detected = 1
+                #cls_id = int(box.cls[0])
+                box = boxes[0]
+                confidence = float(box.conf[0])
+                #name = self.model.names.get(cls_id, str(cls_id))
+                x1, y1, x2, y2 = box.xyxy[0].tolist()
+                width = x2 - x1
+                height = y2 - y1
+                area = (width) * (height)
+
+                #Minimum area
+                if area >= self.min_area:
+                    self.persistence_counter += 1
+                else:
+                    self.persistence_counter = 0
+
+                #Temporal persistence
+                if self.persistence_counter >= self.confirm_frames:
+                    detected = 1
+                else:
+                    detected = 0
+
+                #logging.info("Frame=%d Conf=%.2f Area=%.0f", self.frame_id, confidence, area)
 
         annotated = results[0].plot(img=bgr.copy())  # ready for OpenCV encoding
         #annotated_bgr = cv2.cvtColor(annotated, cv2.COLOR_RGB2BGR)
