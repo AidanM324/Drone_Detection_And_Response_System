43867fd6a33d25c09efb698a5211f4f6c4fde931
Aidan Malone <G00423306@atu.ie>
2026-04-01 14:16:18 +0100

    Got both servo motors running, fixed issues with ground connection

--- servo_test.py
+++ servo_test.py
@@ -1,16 +1,22 @@
 from gpiozero import Servo
+from gpiozero.pins.pigpio import PiGPIOFactory
 from time import sleep
 
-pan = Servo(19)   # X pin
-tilt = Servo(20)  # Y pin
+factory = PiGPIOFactory()
+
+pan = Servo(19, pin_factory=factory)
+tilt = Servo(20, pin_factory=factory)
 
 while True:
+    print("Center")
     pan.value = 0
     tilt.value = 0
     sleep(2)
 
+    print("Left")
     pan.value = -1
     sleep(1)
 
+    print("Right")
     pan.value = 1
     sleep(1)
